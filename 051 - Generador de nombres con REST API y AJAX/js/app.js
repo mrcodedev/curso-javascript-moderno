7da0d86022c53eb171aad9408254e6557e82879a
MrCodeDev <mrcodedev@gmail.com>
2018-08-29 00:46:40 +0200

    Refactorizado ejercicio 051

--- 051 - Generador de nombres con REST API y AJAX/js/app.js
+++ 051 - Generador de nombres con REST API y AJAX/js/app.js
@@ -33,25 +33,17 @@ function cargarNombres(e) {
 
     // crear Fetch
     fetch(url)
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
+        .then(res => res.json())
+        .then(data => {
             let html = `<h2>Nombres Generados</h2>`;
 
             html += `<ul class="lista">`;
             
-            data.forEach((nombre) => {
-                html += `
-                    <li>${nombre.name}</li>
-                `;
-            });
+            data.forEach(nombre => html += `<li>${nombre.name}</li>`);
             
             html += `</ul>`;
 
             document.querySelector('#resultado').innerHTML = html;
         })
-        .catch((error) => {
-            console.log(error);
-        });
+        .catch(error => console.log(error));
 }
